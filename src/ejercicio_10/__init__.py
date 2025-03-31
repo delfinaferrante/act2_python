0cce506bc6914b775433ae8770f9d4ca001ce243
Delfina Ferrante <delfinaferrante1@gmail.com>
2025-03-31 12:43:55 -0300

    Agrego calculo de puntajes de jugadores por rounds

--- src/ejercicio_10/__init__.py
+++ src/ejercicio_10/__init__.py
@@ -1,12 +1,14 @@
 def view_rounds(rounds):
     round_number = 1
     for one_round in rounds:
-        print(f"\n Ronda numero{round_number} \n")
-        print("{:<8} {:<6} {:<12} {:<8}".format("Jugador", "Kills", "Asistencias", "Muertes"))
-        print("-" * 35)
+        print(f"\n Ronda numero {round_number} \n")
+        print("{:<8} {:<6} {:<12} {:<8} {:<8}".format("Jugador", "Kills", "Asistencias", "Muertes", "Puntaje"))
+        print("-" * 45)
 
         for player in one_round:
             data_player = one_round[player]
-            print("{0:6} {1:5d} {2:8d} {3:9d}".format(player, data_player['kills'], data_player['assists'], data_player['deaths']))
+            # Calculo el puntaje total de cada jugador
+            puntaje = (data_player['kills'] * 3) + (data_player['assists'] * 1) - (int(data_player['deaths']) * 1)
+            print("{:<10} {:<6d} {:<12d} {:<7d} {:<8d}".format(player, data_player['kills'], data_player['assists'], data_player['deaths'], puntaje))
         
         round_number += 1
